fbe63c3a245d1e46784b6706b0ff1fa5cb31b172
Yashar Js <Yashar@Icys-MacBook-Pro.local>
2022-03-25 20:58:07 -0400

    contact component completed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,10 +3,11 @@ import {FaDev} from 'react-icons/fa';
 
 export default function About() {
     return (
-        <div className="h-screen flex flex-col items-center space-y-8  p-8 ">
+        <div className="h-screen flex flex-col items-center space-y-8  p-8 text-center ">
             <h1 className="text-4xl font-extrabold">Who am I?</h1>
             <span className="text-4xl"><FaDev></FaDev></span>
-            <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa reprehenderit aspernatur autem a vitae neque placeat accusamus aliquam ut est dolore voluptate, nam distinctio! Iste magni unde fugit similique doloribus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quia nobis saepe iste deleniti illo provident quam optio a earum excepturi mollitia similique soluta quod rem, natus, impedit perspiciatis ipsa!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa reprehenderit aspernatur autem a vitae neque placeat accusamus aliquam ut est dolore voluptate, nam distinctio! Iste magni unde fugit similique doloribus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quia nobis saepe iste deleniti illo provident quam optio a earum excepturi mollitia similique soluta quod rem, natus, impedit perspiciatis ipsa!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum soluta esse non neque labore est et earum enim possimus ipsum vero, molestias perspiciatis, porro rerum tenetur numquam iusto pariatur. A.</p>
         </div>
     )
 }
