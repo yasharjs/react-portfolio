5159fc86d3cd6525887ac9a494ab3b188f93607d
Yashar <yasharjs@gmail.com>
2022-03-31 18:24:42 -0400

    added footer icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {FaGithub, FaGoogle, FaLinkedin} from 'react-icons/fa'
 
 export default function Footer() {
     return (
         <div>
             <footer className="p-5">
-                This is the footer please don't forget about me
+                <span className="text-5xl"><FaGithub></FaGithub></span>
+                <span className="text-5xl"><FaGoogle></FaGoogle></span>
+                <span className="text-5xl"><FaLinkedin></FaLinkedin></span>
+
             </footer>
         </div>
     )
